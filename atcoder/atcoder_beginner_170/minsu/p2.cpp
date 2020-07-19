18806b86a6b80635a12769eb100ef63552f709a3
icycle0409 <icycle0409@snu.ac.kr>
2020-07-19 16:50:19 +0900

    minsu folder updated

---
+++ atcoder/atcoder_beginner_170/minsu/p2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void main(){
+    int x,y;
+    cin>>x;
+    cin>>y;
+    int a=4*x-y;
+    if((a%2==0)&&(a>=0)&&(a<=2*x)) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
