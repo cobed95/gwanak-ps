2e20378e0ce6ea23246c9c377cf665c7c9180687
icycle0409 <icycle0409@snu.ac.kr>
2020-07-19 17:06:50 +0900

    p4 changed

--- atcoder/atcoder_beginner_170/minsu/p4.cpp
+++ atcoder/atcoder_beginner_170/minsu/p4.cpp
@@ -5,15 +5,16 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    vector<int> A,B;
-    int f=0,cnt=0,i,j;
+    vector<int> A;
+    int B[200000];
+    int f=0,i,j;
     for(i=0;i<n;i++){
         int t;
         cin>>t;
-        B.push_back(t);
+        B[i]=t;
         for(j=0;j<f;j++){
             if(A[j]==t){
-                cnt++;
+                break;
             }
             else if(t%A[j]==0){
                 break;
@@ -32,6 +33,19 @@ int main(){
     sort(A.begin(),A.end());
     A.erase(unique(A.begin(),A.end()),A.end());
     f=A.size();
-    
-    cout<<f<<endl;
+    vector<int> fi;
+    for(int i=0;i<f;i++){
+        fi.push_back(0);
+    }
+    for(int i=0;i<f;i++){
+        for(int j=0;j<n;j++){
+            if(A[i]==B[j]) fi[i]++;
+            if(fi[i]>1) break;
+        }
+    }
+    int cnt=0;
+    for(int i=0;i<f;i++){
+        if(fi[i]>1) cnt++;
+    }
+    cout<<f-cnt<<endl;
 }
