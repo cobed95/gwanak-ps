9bb2274bff425520a114962fa6c58d637e5a6eec
icycle0409 <icycle0409@snu.ac.kr>
2020-07-26 16:43:23 +0900

    1,2,3 commited

--- hacker-rank/asia-pacific-4/minsu/p1.cpp
+++ hacker-rank/asia-pacific-4/minsu/p1.cpp
@@ -1,1 +1,22 @@
-qqer`
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char str[1001000];
+    int d;
+    cin>>str>>d;
+    int cnt=0,ans=0;
+    int l=(int)strlen(str);
+    for(int i=0;i<l;i++){
+        if(str[i]=='0'){
+            cnt++;
+        }
+        else{
+            ans+=(cnt/d);
+            cnt=0;
+        }
+    }
+    ans+=(cnt/d);
+    cout<<ans<<endl;
+}
