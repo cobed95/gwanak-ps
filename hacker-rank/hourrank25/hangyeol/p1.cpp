40c26e67873e22e2725b494c955ba737c20c4a95
Hangyeol Park <parkhan00@snu.ac.kr>
2020-08-02 16:16:26 +0900

    p1 AC

---
+++ hacker-rank/hourrank25/hangyeol/p1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+/*
+int main() {
+	int t, n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		int sum = 0;
+		string answer;
+		vector<string> a;
+		for (int j = 0; j < n; j++) {
+			string b;
+			cin >> b;
+			a.push_back(b);
+		}
+		// input
+
+		for (int j = 0; j < a.size(); j++) {
+			for (int k = 0; k < a[j].size(); k++) {
+				if (a[j][k] == '0') {
+					sum += 0;
+				}
+				else {
+					sum += (int)a[j][k];
+				}
+			}
+		}
+		if (sum % 3 == 0) //3ÀÇ ¹è¼ö
+			answer = "YES";
+		else
+			answer = "NO";
+		cout << answer << endl;
+	}
+	
+	return 0;
+}
+*/
