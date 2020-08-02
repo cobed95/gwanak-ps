69c9932f12eff3ffa4f10bd1b7fd1b6202fe3c11
Your Name <you@example.com>
2020-08-02 13:31:40 +0900

    add solution to asiapacific 0726

--- atcoder/0726hkrrnk/hkrnk_asiapacific_0726_doohyung/0726p1.py
+++ atcoder/0726hkrrnk/hkrnk_asiapacific_0726_doohyung/0726p1.py
@@ -1,2 +1,14 @@
-def compress(arr):
-    return(arr)
+def minimumMoves(s, d):
+    n = len(s)
+    conseczeroocc = 0
+    conseczerolist = []
+    for i in range(n-d+1):
+        while s[i]
+
+    # find the occasions of consecutive zeros with length >=d, and identify the length of each occasions
+    # mu = Number of consecutive invalid d-length strings = number of consecutive zeros-d+1
+    # for each occasion of consecutive zeros, the required number of "moves " is len(conseczero)//d
+
+    return(moves)
+
+
---
+++ atcoder/0726hkrrnk/hkrnk_asiapacific_0726_doohyung/0726p2.py
@@ -0,0 +1,15 @@
+def arrangeStudents(a, b):
+    b = ["B"]*len(a)
+    g = ["G"]*len(b)
+
+    boys = {}
+    values = a
+    allinline = sorted(boyinline + girlinline)
+
+    #Given the dictionary with Gender: Height sorted by heights, scan through the sorted dictionary looking for any
+    #consecutive gender keys
+
+
+
+    sol = "True"
+    return sol
